0a98bd879c0c75512ef4f4d499132d3f57d9166d
Yiyi Wang <shd101wyy@sina.com>
2017-02-04 20:54:48 -0600

    added etf for stock

--- scripts/stock/stock.js
+++ scripts/stock/stock.js
@@ -1,65 +1,65 @@
 
-  {
-    target_url: /http\:\/\/money\.cnn\.com\/quote\/quote\.html\?symb\=(.+)/,
-    name: "stock price alert",
-    description: "Check stock price, and alert when necessary.\nFor how to use, please check this [website](https://github.com/shd101wyy/Moni_Sample_Scripts/blob/master/scripts/stock/stock.md)",
-    keywords: ["stock"],
-  	link: "https://github.com/shd101wyy/Moni_Sample_Scripts/blob/master/scripts/stock/stock.md",
-    version: "0.0.2",
-    public: true, // set to true if you want to publish this script so others can find it
-    script: function(event) { // event = {url, cheerio, $get}
+{
+  target_url: /http\:\/\/money\.cnn\.com\/quote\/(etf\/)*(quote|etf)\.html\?symb\=(.+)/,
+  name: "stock price alert",
+  description: "Check stock price, and alert when necessary.\nFor how to use, please check this [website](https://github.com/shd101wyy/Moni_Sample_Scripts/blob/master/scripts/stock/stock.md)",
+  keywords: ["stock"],
+  link: "https://github.com/shd101wyy/Moni_Sample_Scripts/blob/master/scripts/stock/stock.md",
+  version: "0.0.4",
+  public: true,
+  script: function (event) { // event = {url, cheerio, $get}
 	  event.$get({url: event.url, dataType: 'text'}, (error, body)=> {
-        if (error) return event.return(false)
-        const $ = event.cheerio.load(body)
-        const title = $($('.wsod_fLeft')[0]).text()
-        const currentPrice = parseFloat($($('.wsod_quoteData span')[0]).text())
-        const time = $('.wsod_last .wsod_quoteLabel').text().replace('DataAs', 'Data as')
-        const today_s_change = $('.wsod_change').text().replace('%T', '% T')
+      if (error) return event.return(false)
+      const $ = event.cheerio.load(body)
+      const title = $($('.wsod_fLeft')[0]).text()
+      const currentPrice = parseFloat($($('.wsod_quoteData span')[0]).text())
+      const time = $('.wsod_last .wsod_quoteLabel').text().replace('DataAs', 'Data as')
+      const today_s_change = $('.wsod_change').text().replace('%T', '% T')
 
-        let regex = /[?&]([^=#]+)=([^&#]*)/g,
-            params = {},
-            match = null
-        while(match = regex.exec(event.url)) {
-          params[match[1]] = match[2]
-        }
+      let regex = /[?&]([^=#]+)=([^&#]*)/g,
+          params = {},
+          match = null
+      while(match = regex.exec(event.url)) {
+        params[match[1]] = match[2]
+      }
 
-        let max = null
-        if (params['max']) {
-          max = parseFloat(params['max'])
-        }
-        let min = null
-        if (params['min']) {
-          min = parseFloat(params['min'])
-        }
+      let max = null
+      if (params['max']) {
+        max = parseFloat(params['max'])
+      }
+      let min = null
+      if (params['min']) {
+        min = parseFloat(params['min'])
+      }
 
-        let description = `${currentPrice}
+      let description = `${currentPrice}
 ${today_s_change}
 ${time}`
 
-        if (max) {
-          description += '\nalert when >= ' + max
-        }
-        if (min) {
-          description += '\nalert when <= ' + min
-        }
+      if (max) {
+        description += '\nalert when >= ' + max
+      }
+      if (min) {
+        description += '\nalert when <= ' + min
+      }
 
-		let notificationOff = true
-        if (max && currentPrice >= max) {
-          notificationOff = false
-        }
+	    let notificationOff = true
+      if (max && currentPrice >= max) {
+        notificationOff = false
+      }
 
-        if (min && currentPrice <= min) {
-          notificationOff = false
-        }
+      if (min && currentPrice <= min) {
+        notificationOff = false
+      }
 
-        event.return({
-          title: title,
-          description,
-          link: event.url,
-          description,
-          favicon: "chrome://favicon/http://money.cnn.com/",
-          notificationOff
-        })
+      event.return({
+        title: title,
+        description,
+        link: event.url,
+        description,
+        favicon: "chrome://favicon/http://money.cnn.com/",
+        notificationOff
       })
-    }
+    })
   }
+}
