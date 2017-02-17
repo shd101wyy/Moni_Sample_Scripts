d704a35254ea1d48d5c948f9a4e632380b924a45
Yiyi Wang <shd101wyy@sina.com>
2017-02-17 00:15:23 -0600

    fix panda

--- scripts/pandatv.js
+++ scripts/pandatv.js
@@ -1,15 +1,14 @@
+
 {
   target_url: /^(http|https)\:\/\/www\.panda\.tv\/(\d+)/,
   name: "panda tv 主播在线提醒",
   description: "panda tv 主播在线提醒",
-  keywords: [
-    "panda", "tv"
-  ],
-  link: 'https://github.com/shd101wyy/Moni_Sample_Scripts',
-  version: "0.0.1",
-  public: true, // set to true if you want to publish this script so others can find it
-  script: function(event) { // event = {url, cheerio, $get}
-    const roomId = event.url.match(/^(http|https)\:\/\/www\.panda\.tv\/(\d+)/)[1]
+  keywords: ["panda", "tv"],
+  link: "https://github.com/shd101wyy/Moni_Sample_Scripts",
+  version: "0.0.2",
+  public: true,
+  script: function (event) { // event = {url, cheerio, $get}
+    const roomId = event.url.match(/^(http|https)\:\/\/www\.panda\.tv\/(\d+)/)[2]
     event.$get({
       url: `http://www.panda.tv/api_room_v2?roomid=${roomId}`,
       dataType: 'json'
@@ -18,9 +17,9 @@
         return event.return (false)
       if (json.errno !== 0)
         return event.return (false)
-      const videoInfo = json.videoinfo,
-            status = videoinfo.status
-      const hostInfo = json.hostinfo,
+      const videoInfo = json.data.videoinfo,
+            status = videoInfo.status
+      const hostInfo = json.data.hostinfo,
             cover = hostInfo.avatar,
             name = hostInfo.name
       const title = `${name} panda.tv`
